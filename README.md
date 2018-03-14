6892e78a3fd1b82b25dc79fe9208f422d57ff77a
谭林权 <linquantan@gmail.com>
2018-03-14 17:26:56 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # application-health
-application health for node 
+application health for node express
+
+
+## Usage
+
+First, install `application-health`:
+
+```shell
+npm install --save application-health
+```
+
+Then, add it to your express application
+
+```javascript
+var applicationHealth = require("application-health");
+
+app.use(applicationHealth())
+
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+/**
+ *
+ * @param options
+ * @returns {middleware}
+ *
+ */
+function applicationHealth(options) {
+    var opts = options || {}
+    var path = opts.path || '/opsmonitor/health'
+
+    return function applicationHealth(req, res, next) {
+        if (req.url !== path) {
+            next()
+            return
+        }
+
+        if (req.method !== 'GET' && req.method !== 'POST') {
+            res.statusCode = req.method === 'OPTIONS' ? 200 : 405
+            res.setHeader('Allow', 'GET, POST, OPTIONS')
+            res.setHeader('Content-Length', '0')
+            res.end()
+            return
+        }
+
+        res.send({
+            status: 'UP'
+        })
+    };
+}
+
+exports = module.exports = applicationHealth
+
+
